29b88c30d5e8e59393ab3986f7293ce762184a08
blurpit <davidschneider821@gmail.com>
2023-12-06 00:12:51 -0700

    Add definitions to default import

--- calc/__init__.py
+++ calc/__init__.py
@@ -1,2 +1,3 @@
 from .context import Context, ContextError
 from .utils import create_default_context, evaluate, tree, console, graph, latex
+from .definitions import Associativity, DefinitionType, FunctionDefinition, VariableDefinition, DeclaredFunction
